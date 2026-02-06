585e13caea9704048f3847209d352c09e8e7bef5
ThalujaAlwis <125189014+ThalujaAlwis@users.noreply.github.com>
2026-02-06 23:01:27 +0530

    Update learn.js

--- learn.js
+++ learn.js
@@ -1,4 +1,4 @@
-// 1. Variable types
+// 1. Variables
 let name = "Thaluja";
 const age = 22;
 var isStudent = true;
@@ -62,5 +62,6 @@ console.log("Category:", result);
 
 // 10. Basic user interaction (Browser)
 alert("Welcome to JavaScript Basics");
+
 
 
